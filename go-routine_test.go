8c902079cd5142529e356c4e3cf9e853c03a0fd0
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-21 20:19:11 +0700

    Create Go Routines

---
+++ go-routine_test.go
@@ -0,0 +1,19 @@
+package goroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func RunHelloWorld() {
+	fmt.Println("Hello World")
+}
+
+func TestCreateGoRoutines(t *testing.T) {
+	go RunHelloWorld()
+
+	fmt.Println("Ups")
+
+	time.Sleep(1 * time.Second)
+}
