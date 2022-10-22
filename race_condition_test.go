b04144897575bac5c4e059ccf8b3e533f1b3c881
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-22 22:09:29 +0700

    Race Condition Problem

---
+++ race_condition_test.go
@@ -0,0 +1,22 @@
+package goroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestRaceCondition(t *testing.T) {
+	x := 0
+
+	for i := 0; i < 1000; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				x++
+			}
+		}()
+	}
+
+	time.Sleep(1 * time.Second)
+	fmt.Println("Counter : ", x)
+}
