63012949f1dfc51f874112af7139048e024963c1
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-22 23:58:03 +0700

    Read and Write Mutex

--- mutex_test.go
+++ mutex_test.go
@@ -24,3 +24,37 @@ func TestMutex(t *testing.T) {
 	time.Sleep(1 * time.Second)
 	fmt.Println("Counter : ", x)
 }
+
+type BankAccount struct {
+	RWMutex sync.RWMutex
+	balance float64
+}
+
+func (account *BankAccount) AddBalance(amount float64) {
+	account.RWMutex.Lock()
+	account.balance += amount
+	account.RWMutex.Unlock()
+}
+
+func (account *BankAccount) GetBalance() float64 {
+	account.RWMutex.RLock()
+	balance := account.balance
+	account.RWMutex.RUnlock()
+	return balance
+}
+
+func TestRWMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+
+	time.Sleep(1 * time.Second)
+	fmt.Println("Total Balance", account.GetBalance())
+}
