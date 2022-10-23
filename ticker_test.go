b91585524c0b146cab572efd8c4cb7e9b4cd1b74
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-23 16:52:50 +0700

    time.Ticker

---
+++ ticker_test.go
@@ -0,0 +1,28 @@
+package goroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestTicker(t *testing.T) {
+	ticker := time.NewTicker(1 * time.Second)
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		ticker.Stop()
+	}()
+
+	for time := range ticker.C {
+		fmt.Println(time)
+	}
+}
+
+func TestTickFunction(t *testing.T) {
+	channel := time.Tick(1 * time.Second)
+
+	for time := range channel {
+		fmt.Println(time)
+	}
+}
