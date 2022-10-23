a33db1f5b32f0d4f14c17e3d43a2414a9fa0749d
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-23 13:27:25 +0700

    Sync Pool

---
+++ pool_test.go
@@ -0,0 +1,35 @@
+package goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	_ "time"
+)
+
+func TestPool(t *testing.T) {
+	group := sync.WaitGroup{}
+	pool := sync.Pool{
+		New: func() interface{} {
+			return "Vanhautten"
+		},
+	}
+
+	pool.Put("Azie")
+	pool.Put("Melza")
+	pool.Put("Pratama")
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer group.Done()
+			group.Add(1)
+
+			data := pool.Get()
+			// time.Sleep(1 * time.Second)
+			fmt.Println(data)
+			pool.Put(data)
+		}()
+	}
+
+	group.Wait()
+}
